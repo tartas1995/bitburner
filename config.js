91a42bf1f35d828dbb31926e2e556740fa4b8601
tartas1995 <tartas1995@gmail.com>
2021-12-22 12:50:34 +0100

    working on config

--- config.js
+++ config.js
@@ -1,13 +1,29 @@
-import { save, isLoadable } from "./store.js"
+import { save, isLoadable, isLoadable } from "./store.js";
+
+const defaultConfig = {
+    scanDepth: 2
+}
 
 export function init() {
     if (!isLoadable("config")) {
-        save("config", {
-            scanDepth: 2
-        })
+        save("config", defaultConfig);
     }
 }
 
 export function main(ns) {
-    ns.tprint(args)
+    if (!isLoadable("config")) {
+        save("config", defaultConfig);
+    }
+    if (ns.args.length > 0) {
+        let config = load("config");
+        const overwrite = {};
+        for (let arg of args) {
+            const value = arg.split("=")
+            overwrite[value[0]] = value[1]
+        }
+        config = {...config, ...overwrite}
+        save("config", config)
+    } else {
+        ns.tprint(load("config"))
+    }
 }
