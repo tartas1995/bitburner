6afe43d3f1b361b96a8463f7ac236db497fab56d
tartas1995 <tartas1995@gmail.com>
2021-12-22 12:28:32 +0100

    downloader finished

--- downloader.js
+++ downloader.js
@@ -4,12 +4,18 @@ export async function main(ns) {
     }
 }
 
+const baseURL = "https://raw.githubusercontent.com/tartas1995/bitburner/master/"
+
 const files = [
     "init.js",
     "config.js",
 ]
 
 function download(ns, file) {
-    ns.scriptKill(file ,ns.getHostname());
-    ns.rm(file)
+    ns.tprint(`kill ${file}`)
+    await ns.scriptKill(file ,ns.getHostname());
+    ns.tprint(`remove ${file}`)
+    await ns.rm(file);
+    ns.tprint(`download ${file}`)
+    await ns.wget(`${baseURL}${file}`, file);
 }
